a33b1e9b3ebb925f25cd507e04f2dd432ef2f231
kusti420 <kristjan0@live.com>
2022-12-01 14:53:11 +0200

    idk

--- 1/1.1.py
+++ 1/1.1.py
@@ -1,12 +1,4 @@
-import os
-import importlib.util
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-folder = int(os.path.dirname(os.path.realpath(__file__)).split("\\")[-1])
-module = importlib.util.spec_from_file_location("globals", os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\\globals.py")
-globals = importlib.util.module_from_spec(module)
-module.loader.exec_module(globals)
-globals.get_input(folder)
-data = open(f"{folder}.txt", "r", encoding="utf-8", newline="").read().splitlines()
+import os; exec(open(f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}\\setup.txt").read())
 
 if __name__ == "__main__":
     data2 = []
